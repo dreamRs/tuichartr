d0e21db21a40dcd5f7a7d58ed908448383260d9c
pvictor <perrier.victor@gmail.com>
2019-04-30 15:00:09 +0200

    pkgdown

---
+++ docs/articles/starting-with-tuichartr_files/tuichart-binding-0.0.0.9000/tuichart.js
@@ -0,0 +1,68 @@
+HTMLWidgets.widget({
+
+  name: 'tuichart',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    var tuiChart = tui.chart;
+    var widget, options = {}, data = {};
+
+    return {
+
+      renderValue: function(x) {
+
+        var chartType = tuiChart[x.type];
+        if (x.hasOwnProperty('options')) {
+          options = x.options;
+        }
+        if (x.hasOwnProperty('data')) {
+          data = x.data;
+        }
+        if (x.hasOwnProperty('theme')) {
+          tuiChart.registerTheme(x.theme.name, x.theme.values);
+        }
+
+        if (options.hasOwnProperty('chart') === false) {
+          options.chart = {};
+        }
+        options.chart.width = width;
+        options.chart.height = height;
+
+        widget = chartType(el, data, options);
+
+      },
+
+      getWidget: function(){
+        return widget;
+      },
+
+      resize: function(width, height) {
+
+        widget.resize({width: width, height: height});
+
+      }
+
+    };
+  }
+});
+
+
+// From Friss tuto (https://github.com/FrissAnalytics/shinyJsTutorials/blob/master/tutorials/tutorial_03.Rmd)
+function get_widget(id){
+
+  // Get the HTMLWidgets object
+  var htmlWidgetsObj = HTMLWidgets.find("#" + id);
+
+  // Use the getWidget method we created to get the underlying widget
+  var widgetObj ;
+
+  if (typeof htmlWidgetsObj != 'undefined') {
+    widgetObj = htmlWidgetsObj.getWidget();
+  }
+
+  return(widgetObj);
+}
+
+
